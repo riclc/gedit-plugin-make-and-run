260609d7b5bf8f613dc8c23bbe27a8c7afff2985
asdf.lenz <asdf.lenz@4f5a5a58-66d5-11de-ba0c-230037762ff9>
2009-07-04 10:17:51 +0000

    Now uses gconf to load and store settings.

--- MakeAndRun/mr_globals.py
+++ MakeAndRun/mr_globals.py
@@ -7,6 +7,7 @@
 # see README file
 
 import os.path
+import gconf
 
 GLADE_FILE_CONFIG = os.path.join( os.path.dirname( __file__ ), "gui_config.glade" )
 GLADE_FILE_OUTPUT = os.path.join( os.path.dirname( __file__ ), "gui_output.glade" )
@@ -22,6 +23,80 @@ IMG_GEDIT = os.path.join( os.path.dirname( __file__ ), "imgs", "iconGedit.png" )
 IMG_MAKE = os.path.join( os.path.dirname( __file__ ), "imgs", "iconMake.png" )
 IMG_PYTHON_BIG = os.path.join( os.path.dirname( __file__ ), "imgs", "iconPythonBig.png" )
 
-# opcao default: mostra console
-checked_show_terminal = True
+GCONF_DIR = "/apps/gedit-2/plugins/make-and-run/"
 
+
+class configurations:
+    compile_c = "gcc -c"
+    compile_cpp = "g++ -c"
+    compile_python = "pyflakes"
+    compile_autosave = True
+
+    cmd_make_exec = "exec"
+    show_terminal = True
+    run_python_thru_make_exec = False
+    run_python_auto_close_window = True
+
+    bottom_panel_size = 200
+    bottom_panel_size_ignore = False
+
+
+    @staticmethod
+    def load():
+        gclient = gconf.client_get_default()
+
+
+        try:
+            configurations.compile_c = gclient.get( \
+                GCONF_DIR + "compile_c" ).get_string()
+            configurations.compile_cpp = gclient.get( \
+                GCONF_DIR + "compile_cpp" ).get_string()
+            configurations.compile_python = gclient.get( \
+                GCONF_DIR + "compile_python" ).get_string()
+            configurations.compile_autosave = gclient.get( \
+                GCONF_DIR + "compile_autosave" ).get_bool()
+
+            configurations.cmd_make_exec = gclient.get( \
+                GCONF_DIR + "cmd_make_exec" ).get_string()
+            configurations.show_terminal = gclient.get( \
+                GCONF_DIR + "show_terminal" ).get_bool()
+            configurations.run_python_thru_make_exec = gclient.get( \
+                GCONF_DIR + "run_python_thru_make_exec" ).get_bool()
+            configurations.run_python_auto_close_window = gclient.get( \
+                GCONF_DIR + "run_python_auto_close_window" ).get_bool()
+
+            configurations.bottom_panel_size = gclient.get( \
+                GCONF_DIR + "bottom_panel_size" ).get_int()
+            configurations.bottom_panel_size_ignore = gclient.get( \
+                GCONF_DIR + "bottom_panel_size_ignore" ).get_bool()
+        except:
+            pass
+
+
+
+    @staticmethod
+    def save():
+        gclient = gconf.client_get_default()
+
+        gclient.set_string( GCONF_DIR + "compile_c", \
+            configurations.compile_c )
+        gclient.set_string( GCONF_DIR + "compile_cpp", \
+            configurations.compile_cpp )
+        gclient.set_string( GCONF_DIR + "compile_python", \
+            configurations.compile_python )
+        gclient.set_bool( GCONF_DIR + "compile_autosave", \
+            configurations.compile_autosave )
+
+        gclient.set_string( GCONF_DIR + "cmd_make_exec", \
+            configurations.cmd_make_exec )
+        gclient.set_bool( GCONF_DIR + "show_terminal", \
+            configurations.show_terminal )
+        gclient.set_bool( GCONF_DIR + "run_python_thru_make_exec", \
+            configurations.run_python_thru_make_exec )
+        gclient.set_bool( GCONF_DIR + "run_python_auto_close_window", \
+            configurations.run_python_auto_close_window )
+
+        gclient.set_int( GCONF_DIR + "bottom_panel_size", \
+            configurations.bottom_panel_size )
+        gclient.set_bool( GCONF_DIR + "bottom_panel_size_ignore", \
+            configurations.bottom_panel_size_ignore )
